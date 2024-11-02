59e5fb902c972332139657c2a3e1cc7c4ec0ee77
ClaudioCumbene <ClaudioCumbene@gmail>
2024-11-02 16:29:33 +0200

    First steps in java with spring boot 4

---
+++ 05_FirstStepsWithSpringBoot/rest-with-spring-boot-and-java-oidu/src/test/java/com/techoidu/StartupTests.java
@@ -0,0 +1,13 @@
+package com.techoidu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
